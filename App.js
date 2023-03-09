c35daec44dcda5b9eb9f9870f39e76c9d1901550
alejandrabarriost <93233927+alejandrabarriost@users.noreply.github.com>
2023-03-08 20:42:14 -0500

    Parcial

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "./components/Layout.js";
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+       <Layout/>
+      </div>
+    )
+  };
+
+}
+
+export default App;
